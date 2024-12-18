b9666b12114163c69a5ac251721ffe1aa8370464
Carson Powers <me@carsonp.net>
2024-12-17 20:35:56 -0600

    move dirs

--- 1431.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-  def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-    mostCandies = max(candies)
-    return [c + extraCandies >= mostCandies for c in candies]
--- 151.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-  def reverseWords(self, s: str) -> str:
-    # Split the string into a list every time we hit a space
-    words = s.split()
-    # Join words, reversed, with a space inbetween
-    return ' '.join(words[::-1])
